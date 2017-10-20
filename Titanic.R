4c6932f287ba82daf5c1e1cc2d8614a99e3ee5f2
rebelee <rebelee@outlook.com>
2017-10-19 19:35:35 -0500

    edited to work with own titanic data set

--- Titanic.R
+++ Titanic.R
@@ -1,22 +1,31 @@
-# titanic is avaliable in your workspace
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+# load data into RStudio, clean titanic version from previous project
+titanic <- read.csv("titanic_clean.csv")
+
+# replace missing values in survived column with 0
+titanic <- titanic %>%
+  mutate(survived = replace(survived, is.na(survived), 0L))
+
 # 1 - Check the structure of titanic
+# is the data tidy? 
 str(titanic)
 
-# 2 - Use ggplot() for the first instruction
-ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+# 2 - Plot distribution of the sexes within the classes of the ship
+ggplot(titanic, aes(x = pclass, fill = sex)) +
   geom_bar(position = "dodge")
 
-# 3 - Plot 2, add facet_grid() layer
-ggplot(titanic, aes(x = Pclass, fill = Sex)) +
+# 3 - Adding facet_grid layer since previous bar plot won't help estimate chance of survival
+ggplot(titanic, aes(x = pclass, fill = sex)) +
   geom_bar(position = "dodge") +
-  facet_grid(. ~ Survived)
+  facet_grid(. ~ survived)
 
 # 4 - Define an object for position jitterdodge, to use below
 posn.jd <- position_jitterdodge(0.5, 0, 0.6)
 
-# 5 - Plot 3, but use the position object from instruction 4
-ggplot(titanic, aes(x = Pclass, y = Age, col = Sex)) +
+# 5 - Add the final variable Age, and use the position object described above
+ggplot(titanic, aes(x = pclass, y = age, col = sex)) +
   geom_point(size = 3, alpha = 0.5, position = posn.jd) +
-  facet_grid(. ~ Survived)
-
-
+  facet_grid(. ~ survived)
